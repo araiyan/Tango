9a27e47095230d1577fa731722f91f934f79608b
Kester <kestertan040@gmail.com>
2025-02-09 13:06:38 -0500

    reverted threading

--- vmms/ec2SSH.py
+++ vmms/ec2SSH.py
@@ -541,33 +541,21 @@ class Ec2SSH(object):
         if not inputFiles or not all(hasattr(file, 'localFile') and hasattr(file, 'destFile') for file in inputFiles):
             self.log.info("Error: Invalid inputFiles Structure, job: %s" % (job_id))
 
-        def scp_file(file):
-            self.log.info("Copying %s to %s" % (file.localFile, file.destFile)) 
+        
+        for file in inputFiles:
+            self.log.info("%s - %s" % (file.localFile, file.destFile))
             ret = timeout_with_retries(
                 ["scp"]
                 + self.ssh_flags
                 + [
                     file.localFile,
-                    f"{self.ec2User}@{domain_name}:~/autolab/{file.destFile}",
+                    "%s@%s:~/autolab/%s" % (self.ec2User, domain_name, file.destFile),
                 ],
                 config.Config.COPYIN_TIMEOUT,
             )
             if ret != 0:
-                # why did the scp fail
-                self.log.error("Failed to copy file %s to %s" (file.localFile, file.destFile))
-            return ret
-        
-        # TODO: Not sure what max-concurrent should be here
-        max_concurrent = 5
-
-        # Limit the number of concurrent SCP commands
-        with ThreadPoolExecutor(max_workers=max_concurrent) as executor:
-            results = list(executor.map(scp_file, inputFiles))
-
-        # Check for failures
-        if any(ret != 0 for ret in results):
-            self.log.info("Copy-in Error: One or more files failed to copy")
-            return -1
+                self.log.info("Copy-in Error: SCP failure, job: %s", job_id)
+                return ret
 
         return 0
 
